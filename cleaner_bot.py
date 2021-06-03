4bef704b922039e48ef3db3a5234150c569b2347
cobaltCorsair <cobaltcorsair@yandex.ru>
2021-06-03 12:58:47 +0300

    Add comments

--- cleaner_bot.py
+++ cleaner_bot.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 
 
 class Daemon:
+    """Класс бота-клинера"""
     def __init__(self, path):
         self.deleted_files = 0
         self.deleted_files_list = []
@@ -11,6 +12,7 @@ class Daemon:
         self.extensions = ['.log', '.root', '.doses']
 
     def check_folder(self):
+        """Метод проверки файлов папки на совпадение с условиями"""
         for filename in os.listdir(self.folder):
             file_path = os.path.join(self.folder, filename)
             file, file_extension = os.path.splitext(filename)
@@ -20,12 +22,14 @@ class Daemon:
         self.create_log()
 
     def delete_file(self, filepath, last_time_mod):
+        """Метод для удаления файлов"""
         if os.path.isfile(filepath):
             os.remove(filepath)
             self.deleted_files += 1
             self.deleted_files_list.append(filepath + " - last modification: " + str(last_time_mod.isoformat(' ', 'seconds')))
 
     def create_log(self):
+        """Метод, записывающий лог-файл"""
         logs = {"Deleted files": self.deleted_files,
                 "Date of last cleaning": self.this_date.isoformat(' ', 'seconds')}
 
@@ -41,5 +45,6 @@ class Daemon:
                     out_add.write('\n')
 
 
+# путь к папке, которую будем чистить
 folder_path = r'D:\testfolder'
 Daemon(folder_path).check_folder()
